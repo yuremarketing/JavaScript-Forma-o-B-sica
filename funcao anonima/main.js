ce276426275e4d0694fef8d7f609468466475b45
Yure Mark <yure.mark@gmail.com>
2022-04-05 15:47:48 -0300

    exemplo de fuinção anonima

---
+++ funcao anonima/main.js
@@ -0,0 +1,16 @@
+/* exemplo de função anônima, que são funções 
+que por não terem nomes declarados, devem estar vincualadas a uma 
+variável, evento ou algo semelhante. 
+*/
+
+
+a = 5/7;
+b= 18/25;
+
+var theBiggest = function(){
+
+    var result;
+    a>b? result = ['a', a] : result = ['b', b];
+    console.log(result);
+};
+theBiggest();
