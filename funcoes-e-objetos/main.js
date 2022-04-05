94ca9c6a1e8c633dbfb7139d9dca05c86b83992e
Yure Mark <yure.mark@gmail.com>
2022-04-05 13:24:43 -0300

    exemplo de função com parâmetros

---
+++ funcoes-e-objetos/main.js
@@ -0,0 +1,12 @@
+//exemplo de função com passagem de parâmetros, 
+//neste exemplo, temos 2 argumentos, nota que esses parâmetros, não tem relação 
+//com as variáveis que estão fora
+
+function getMaior(a,b ){
+    a < b ? console.log("a:" ,a) : console.log("b :",b)
+}
+
+/* var a = 'b';
+var b = 'a';
+ */
+getMaior('b','1');
