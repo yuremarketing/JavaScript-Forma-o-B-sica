439358f343338a050b24624133cc5d60400bf901
Yure Mark <yure.mark@gmail.com>
2022-04-07 00:13:23 -0300

    atribuindo DATE(); à função)

--- app-clock/script.js
+++ app-clock/script.js
@@ -1,3 +1,20 @@
 const PONTEIROHORA = document.querySelector("#hour");
 const PONTEIROMINUTO = document.querySelector("#minute");
 const PONTEIROSEGUNDO = document.querySelector("#second");
+
+
+var data = new Date();
+
+let hr = data.getHours();
+let min= data.getMinutes();
+let seg= data.getSeconds();
+
+
+console.log("hora: " + hr, "\nminuto: " + min, "\nsegundo: " + seg);
+
+
+/* nota que se lera a sentença assim:
+transforme o estilo da constatnte criada */
+PONTEIROHORA.style.transform = "rotate("+hr+"deg)";
+PONTEIROMINUTO.style.transform = "rotate("+min+"deg)";
+PONTEIROSEGUNDO.style.transform = "rotate("+seg+"deg)";
