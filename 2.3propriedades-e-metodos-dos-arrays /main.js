6861ac9b3474f0f7984f8ce2b7f671defddbfd79
Yure Mark <yure.mark@gmail.com>
2022-04-06 23:08:11 -0300

    subindo exercicios e conteúdos

---
+++ 2.3propriedades-e-metodos-dos-arrays /main.js
@@ -0,0 +1,37 @@
+var pens;
+//Declaração de um array 
+pens = ["yellow", "blue","red"];
+//console.log(pens)
+
+//tratar o tamanho do array
+console.log("length :", pens.length)
+
+//metodo reverse, basicamente reverte a ordem do array
+//let a = pens.reverse();
+//console.log(a);
+
+
+//metodo remove a 1a posição do array
+//pens.shift();
+
+//metodo adiciona um ou mais elementos no array
+//pens.unshift("black", "brow");
+
+//metodo remove o último elemetno do array
+//pens.pop();
+
+//metodo adiciona uma lisa de valores ao final
+//pens.push("pink", "dark blue");
+
+//metodo remove N numeros de ítens de uma posicão
+//pens.splice(3,1);
+
+//metodo que junta os valores dentro um array e transforma eles em 
+//uma String separada por vírgulas, basicamente ele faz um splice do array
+//e o retorna separado com o que vc determinar, pode nser vírgula, espaço etc.
+
+
+
+
+
+console.log("mostrar:",pens);
