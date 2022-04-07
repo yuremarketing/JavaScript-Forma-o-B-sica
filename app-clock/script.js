562b3d131523d44461711d753948045c39c0ba6b
Yure Mark <yure.mark@gmail.com>
2022-04-07 00:52:51 -0300

    ajuste app

--- app-clock/script.js
+++ app-clock/script.js
@@ -3,6 +3,7 @@ const PONTEIROMINUTO = document.querySelector("#minute");
 const PONTEIROSEGUNDO = document.querySelector("#second");
 
 
+
 var data = new Date();
 
 let hr = data.getHours();
@@ -10,7 +11,7 @@ let min= data.getMinutes();
 let seg= data.getSeconds();
 
 
-console.log("hora: " + hr, "\nminuto: " + min, "\nsegundo: " + seg);
+/* console.log("hora: " + hr, "\nminuto: " + min, "\nsegundo: " + seg); */
 
 
 /* convertendo a data atual em graus, de forma a corresponder 
@@ -20,8 +21,18 @@ let posicaoHr   = (hr*360/12)+(min*(360/60)/12);
 let posicaoMim  =(min*360/60) + (seg*(360/60)/60); 
 let posicaoSeg  = seg*360/60;
 
-/* nota que se lera a sentença assim:
-transforme o estilo da constatnte criada */
-PONTEIROHORA.style.transform = "rotate("+posicaoHr+"deg)";
-PONTEIROMINUTO.style.transform = "rotate("+posicaoMim+"deg)";
-PONTEIROSEGUNDO.style.transform = "rotate("+posicaoSeg+"deg)";
+function executaRelogio(){
+
+    posicaoHr  = posicaoHr+(3/360);
+    posicaoMim = posicaoMim+(6/60);
+    posicaoSeg = posicaoSeg+6;
+
+    /* nota que se lera a sentença assim:
+    transforme o estilo da constatnte criada */
+    PONTEIROHORA.style.transform = "rotate("+posicaoHr+"deg)";
+    PONTEIROMINUTO.style.transform = "rotate("+posicaoMim+"deg)";
+    PONTEIROSEGUNDO.style.transform = "rotate("+posicaoSeg+"deg)";
+
+}
+
+var intervalo  =  setInterval(executaRelogio, 1000 );
